2a8abea627875be380cadc26568a2a7f9e3574cd
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-11-07 19:28:20 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// function fetchCountries(name) {
+//     return fetch(
+//         `https://restcountries.com/v3.1/name/peru`
+//     )
+//         .then(response => response.json())
+//         .then (data => console.log(data));
+// }
+
+// console.log(fetchCountries());
+
+// // console.log(fetchCountries(pery));
+// // // function fetchCountries(name) {
+// // //     return fetch(
+// // //         `https://restcountries.com/v3.1/name/{name}`
+// // //     )
+// // //         .then(response => response.json())
+// // //         .then (data => console.log(data));
+// // // }
+
+
